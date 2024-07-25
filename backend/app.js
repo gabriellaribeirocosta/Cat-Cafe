6fc19d7e196dbcaa03c65352afdfc257d5d627fc
Gabriella Costa <gabx2205@gmail.com>
2024-07-24 21:50:38 -0300

    feat: database connection config

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
-import express from 'express'
-import bodyParser from 'body-parser'
+import express from 'express';
+import bodyParser from 'body-parser';
+import sequelize from './database/db-config.js'
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-const PORT = 3000
+const PORT = 3000;
+
 app.listen(PORT, () => {
   console.info(`Server running on http://localhost:${PORT}`)
-})
+});
