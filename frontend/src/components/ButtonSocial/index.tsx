1ff8b5064e96d6864406b7fc07735d8cf5fb1166
ViniciusCampeao <viniciusfernandes643@gmail.com>
2024-07-26 18:43:38 -0300

    feat: completed social button

---
+++ frontend/src/components/ButtonSocial/index.tsx
@@ -0,0 +1,20 @@
+import './style.css'
+import { ReactNode } from 'react'
+
+interface ButtonProps {
+  children: ReactNode
+  onClick: () => void
+  className?: string
+  logo?: string
+}
+
+const Button = ({ children, onClick, className, logo }: ButtonProps) => {
+  return (
+    <button className={`button ${className}`} onClick={onClick}>
+      <div>{logo && <img src={logo} alt="Logo" className="button_logo" />}</div>
+      <div>{children}</div>
+    </button>
+  )
+}
+
+export default Button
