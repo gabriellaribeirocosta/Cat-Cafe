913719242d6c3f3ce909271da52e99a2d2f8e91e
Daniel-Lzs <dan201619@gmail.com>
2024-07-31 13:47:03 -0300

    fix(route-guard): remove elements of pages

--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -1,7 +1,5 @@
 import React from "react"
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import Login from "../pages/Login/Login"
-import Dashboard from "../pages/Dashboard/Dashboard"
 import PrivateRoutes from "./PrivateRoutes"
 import { AuthContextProvider } from "../contexts/AuthContext"
 import PublicRoutes from "./PublicRoutes"
@@ -12,11 +10,11 @@ const index:React.FC = () => {
     <BrowserRouter>
       <Routes>
         <Route element={<PublicRoutes/>}>
-          <Route path="/login" element={<Login/>}/>
+          <Route path="/login" element={"insert-page-products"}/>
         </Route>
         <Route element={<PrivateRoutes/>}>
-          <Route path="/" element={<Dashboard/>}/>
-          <Route path="/products" element={"insert-component-products"}/>
+          <Route path="/" element={"insert-page-products"}/>
+          <Route path="/products" element={"insert-page-products"}/>
           <Route path="*" element={<Navigate to="/"/>}/>
         </Route>
       </Routes>
