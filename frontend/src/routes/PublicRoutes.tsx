f916a8edb080e6883288d7ef95a0e976018f7d83
Daniel-Lzs <dan201619@gmail.com>
2024-07-31 13:29:57 -0300

    feat(route-guard): add PublicRoutes to manage public routes and handle authenticated redirection

---
+++ frontend/src/routes/PublicRoutes.tsx
@@ -0,0 +1,15 @@
+import { Outlet, Navigate } from 'react-router-dom'
+import { AuthContext } from '../contexts/AuthContext';
+import { useContext } from 'react';
+
+const PublicRoutes: React.FC = () => {
+  const {user, loading} = useContext(AuthContext)
+
+  if (loading) {
+    return <></>;
+  }
+
+  return user ? <Navigate to="/"/> : <Outlet />;
+};
+
+export default PublicRoutes
