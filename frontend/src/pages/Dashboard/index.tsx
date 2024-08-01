37620203d273fd19c754503c85eda6f0493c55a7
Gabriel Sousa <gabrielsarubo@hotmail.com>
2024-08-01 16:11:11 -0300

    feat: create Dashboard page

---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import style from './style.module.css'
+
+const Dashboard = () => {
+  return (
+    <section className={style.Dashboard}>
+      <div className={style.CardsList}></div>
+    </section>
+  )
+}
+
+export default Dashboard
