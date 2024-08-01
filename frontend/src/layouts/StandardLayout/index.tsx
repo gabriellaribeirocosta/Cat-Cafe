95599ee1295d482d3aa1fd8357b13cae49e90aa9
Gabriel Sousa <gabrielsarubo@hotmail.com>
2024-08-01 12:29:57 -0300

    feat: add Sidebar and Header to Stardard layout

--- frontend/src/layouts/StandardLayout/index.tsx
+++ frontend/src/layouts/StandardLayout/index.tsx
@@ -1,13 +1,21 @@
 import { Outlet } from 'react-router-dom'
+
 import MainContent from '../../components/MainContent'
+import Sidebar from '../../components/Sidebar'
+import Header from '../../components/Header'
+
+import style from './style.module.css'
 
 const StandardLayout = () => {
   return (
     <>
-      <aside>Sidebar</aside>
-      <MainContent>
-        <Outlet />
-      </MainContent>
+      <Sidebar />
+      <div className={style.StandardLayout}>
+        <Header />
+        <MainContent>
+          <Outlet />
+        </MainContent>
+      </div>
     </>
   )
 }
