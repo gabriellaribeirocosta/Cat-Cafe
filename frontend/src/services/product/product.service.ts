0fc699a09f5adc37987ab674d3bae12bbc26c025
Gabriel Sousa <gabrielsarubo@hotmail.com>
2024-08-02 21:43:25 -0300

    fix: change URL to match API endpoint

--- frontend/src/services/product/product.service.ts
+++ frontend/src/services/product/product.service.ts
@@ -3,7 +3,7 @@ import http from '../../http-common'
 import { ProductProps } from '../../interfaces/Product'
 
 const findAll = async () => {
-  const response = await http.get<ProductProps[]>('products')
+  const response = await http.get<ProductProps[]>('product')
   return response.data
 }
 
