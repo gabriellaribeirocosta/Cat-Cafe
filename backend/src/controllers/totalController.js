c59263556d0f32192fb9846eed3359667f3f570e
Daniel-Lzs <dan201619@gmail.com>
2024-08-03 21:21:54 -0300

    feat(token-auth): add filtering by user_id and rename function

--- backend/src/controllers/totalController.js
+++ backend/src/controllers/totalController.js
@@ -1,6 +1,7 @@
-export const getTotalController = (model) => async (req, res) => {
+export const totalController = (model) => async (req, res) => {
   try {
-    const total = await model.count()
+    const { user_id } = req.user
+    const total = await model.count({ where: { user_id: user_id } })
     res.status(200).json(total)
   } catch (error) {
     res.status(400).json({ error: 'Model not found' })
