d138d7fa4ded9ad2a924e662e707f94e5eb5c15b
Daniel-Lzs <dan201619@gmail.com>
2024-08-04 23:25:06 -0300

    fix: add interceptor to pass the Id token in the header of every application request

--- frontend/src/http-common.ts
+++ frontend/src/http-common.ts
@@ -1,8 +1,19 @@
 import axios from 'axios'
+import { authService } from './services/firebase/auth/authService'
 
-export default axios.create({
+const api = axios.create({
   baseURL: import.meta.env.VITE_APP_API_BACKEND ?? 'http://localhost:3000/',
   headers: {
     'Content-Type': 'application/json',
   },
 })
+
+api.interceptors.request.use(async (config) => {
+  const token = await authService.userIdToken()
+  if (token) {
+    config.headers.Authorization = `${token}`
+  }
+  return config
+})
+
+export default api
