770dd3b6683fe4ade20c5d4d09697e41ab71321a
Gabriel Sousa <gabrielsarubo@hotmail.com>
2024-08-05 14:45:27 -0300

    feat: add link to Products page from Dashboard card

--- frontend/src/components/CardDashboard/index.tsx
+++ frontend/src/components/CardDashboard/index.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react'
+import { Link } from 'react-router-dom'
 import styles from './style.module.css'
 
 interface CardDashboardProps {
@@ -12,16 +13,18 @@ const CardDashboard = ({
   children,
   className,
   logo,
-  number
+  number,
 }: CardDashboardProps) => {
   return (
-    <button className={`${styles.Card} ${className}`}>
-      <div className={styles.CardBox}>
-        {logo && <img src={logo} alt="Logo" className={styles.card_logo} />}
-        <span className={styles.children}>{children}</span>
-      </div>
-      {number !== undefined && <div className={styles.number}>{number}</div>}
-    </button>
+    <Link to="/products" title="Products page">
+      <button className={`${styles.Card} ${className}`}>
+        <div className={styles.CardBox}>
+          {logo && <img src={logo} alt="Logo" className={styles.card_logo} />}
+          <span className={styles.children}>{children}</span>
+        </div>
+        {number !== undefined && <div className={styles.number}>{number}</div>}
+      </button>
+    </Link>
   )
 }
 
